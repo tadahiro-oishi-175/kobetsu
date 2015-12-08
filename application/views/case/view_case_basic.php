eda32d6b909123759a919ec188498a4c1e1d0ea4
tadahiro-oishi-175 <arms175@cb4.so-net.ne.jp>
2015-12-08 22:48:54 +0900

    20151208

---
+++ application/views/case/view_case_basic.php
@@ -0,0 +1,17 @@
+<table class="InputForm">
+    <tr>
+        <th nowrap>RQ No</th>
+        <td><?= $caseObj->CaseNo ?></td>
+    </tr>
+
+    <tr>
+        <th nowrap>案件名</th>
+        <td><?= $caseObj->CaseTitle ?></td>
+    </tr>
+
+    <tr>
+        <th nowrap>顧客名</th>
+        <td><?= $caseObj->CustomerName ?></td>
+    </tr>
+</table>
+<br>
