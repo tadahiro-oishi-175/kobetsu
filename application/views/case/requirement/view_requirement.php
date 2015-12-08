4ec02c63b7342f400d24d856d223a294e780bf82
tadahiro-oishi-175 <arms175@cb4.so-net.ne.jp>
2015-12-08 19:32:29 +0900

    20151208_01

--- application/views/case/requirement/view_requirement.php
+++ application/views/case/requirement/view_requirement.php
@@ -10,9 +10,123 @@ and open the template in the editor.
         <link href="<?= base_url() ?>application/css/style.css" rel="stylesheet" type="text/css"/>
         <script src="<?= base_url() ?>application/js/jquery-2.1.4.min.js" type="text/javascript" charset="utf-8"></script>
         <title>View Requirement</title>
+        <style>
+            .track-progress[data-steps="3"] li { width: 20%; }
+            .track-progress[data-steps="4"] li { width: 18%; }
+            .track-progress[data-steps="5"] li { width: 15%; }
+            .track-progress[data-steps="6"] li { width: 15%; }
+
+            .track-progress {
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+            }
+
+            .track-progress li > span {
+                display: block;
+
+                color: #999;
+                font-weight: bold;
+                text-transform: uppercase;
+            }
+
+            .track-progress li.done > span {
+                color: #666;
+                background-color: #ccc;
+            }
+
+            .track-progress li {
+                list-style-type: none;
+                display: inline-block;
+
+                position: relative;
+                margin: 0;
+                padding: 0;
+
+                text-align: center;
+                line-height: 30px;
+                height: 30px;
+
+                background-color: #f0f0f0;
+            }
+            .track-progress li > span:after,
+            .track-progress li > span:before {
+                content: "";
+                display: block;
+                width: 0px;
+                height: 0px;
+
+                position: absolute;
+                top: 0;
+                left: 0;
+
+                border: solid transparent;
+                border-left-color: #f0f0f0;
+                border-width: 15px;
+            }
+
+            .track-progress li > span:after {
+                top: -5px;
+                z-index: 1;
+                border-left-color: white;
+                border-width: 20px;
+            }
+
+            .track-progress li > span:before {
+                z-index: 2;
+            }
+            .track-progress li.done + li > span:before {
+                border-left-color: #ccc;
+            }
+
+            .track-progress li:first-child > span:after,
+            .track-progress li:first-child > span:before {
+                display: none;
+            }
+            .track-progress li:first-child i,
+            .track-progress li:last-child i {
+                display: block;
+                height: 0;
+                width: 0;
+
+                position: absolute;
+                top: 0;
+                left: 0;
+
+                border: solid transparent;
+                border-left-color: white;
+                border-width: 15px;
+            }
+
+            .track-progress li:last-child i {
+                left: auto;
+                right: -15px;
+
+                border-left-color: transparent;
+                border-top-color: white;
+                border-bottom-color: white;
+            }
+        </style>
     </head>
     <body>
         <?php echo form_open("RequirementController/EditRequirement/$reqObj->RequirementID"); ?>
+        <ol class="track-progress" data-steps="4">
+            <li class="done">
+                <span>1. First Step</span>
+                <i></i>
+            </li><!--
+            --><li class="done">
+                <span>2. Second Step</span>
+            </li><!--
+            --><li class="done">
+                <span><a href="#">3. Third Step</a></span>
+                <i></i>
+            </li><!--
+            --><li>
+                <span>Final Details2</span>
+                <i></i>
+            </li>
+        </ol>
         <div id="main">
             <label>案件基本情報</label>
             <table class="InputForm">
